712770733cb375d66b7ad3380bb2b96be6b1db7d
Besufikad Micheal <64811433+Besufikad17@users.noreply.github.com>
2021-01-20 03:31:37 +0300

    Add files via upload

---
+++ challenge/src/components/Jobs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const Jobs = () => {
+    return(
+        <div>
+            <Header/>
+            <h1></h1>
+        </div>
+        
+    )
+}
+
+export default Jobs;
